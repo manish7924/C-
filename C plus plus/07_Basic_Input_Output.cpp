aacbf136c3e4d40386e83acce2a3a9c99e7f28a9
Ir⎊nic7 Man <114106502+manish7924@users.noreply.github.com>
2025-08-03 03:43:12 +0530

    Create 07_Basic_Input_Output.cpp

---
+++ C plus plus/07_Basic_Input_Output.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;                        // Getting input from the user
+    cout << "Your number is: " << num; // Displaying the input value
+    return 0;
+}
