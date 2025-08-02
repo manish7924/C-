b504353d7e362a60c079f3c9e6c97b4b99f31920
Ir⎊nic7 Man <114106502+manish7924@users.noreply.github.com>
2025-08-03 04:08:40 +0530

    Create 16_01_Structures.cpp

---
+++ C plus plus/16_01_Structures.cpp
@@ -0,0 +1,19 @@
+// Creating Struct Elements 
+
+#include <iostream>
+using namespace std;
+
+struct employee
+{
+    /* data */
+    int eId;
+    char favChar;
+    int salary;
+};
+
+int main()
+{
+    struct employee Harry;
+    return 0;
+}
+
