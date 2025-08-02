e14250b741e305fbf5b7f5db33441a4f8f23b4de
Ir⎊nic7 Man <114106502+manish7924@users.noreply.github.com>
2025-08-03 04:05:10 +0530

    Create 15_Strings.cpp

---
+++ C plus plus/15_Strings.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+    // declare and initialize string
+    string str = "CodeWithHarry";
+    cout << str << endl;
+    return 0;
+}
