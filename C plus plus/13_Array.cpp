04f9c3f411453e67fc24bcf83fc55c025e45f1c4
Ir⎊nic7 Man <114106502+manish7924@users.noreply.github.com>
2025-08-03 03:56:42 +0530

    Create 13_Array.cpp

---
+++ C plus plus/13_Array.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    // creating array
+    // Defining an array
+    // Without specifying the size of the array:
+    int arr[] = {1, 2, 3};
+
+    // With specifying the size of the array:
+    int arr[3];
+    arr[0] = 1, arr[1] = 2, arr[2] = 3;
+
+    // Accessing an array element
+    int arr[] = {1, 2, 3};
+    cout << arr[1] << endl;
+
+    // Changing an array element
+    int arr[] = {1, 2, 3};
+    arr[2] = 8; // changing the element on index 2
+    cout << arr[2] << endl;
+  
+
+    
+}
